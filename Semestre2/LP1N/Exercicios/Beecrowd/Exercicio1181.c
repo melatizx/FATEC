6c7c52244eb62cc1aae21982f4950b328bb1dc82
melatizx <leo.ambrosio0107@gmail.com>
2026-08-17 21:33:38 -0300

    .

--- Semestre2/LP1N/Exercicios/Beecrowd/Exercicio1181.c
+++ Semestre2/LP1N/Exercicios/Beecrowd/Exercicio1181.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
-// int fSoma(int matriz[][], int l)
-// {
-//     int soma = 0, c = 0;
-//     for (c = 0; c < 12; c++)
-//     {
-//         soma = soma + matriz[l][c];
-//     }
-//     return soma;
-// }
+int fSoma(int matriz[][12], int l)
+{
+    int soma = 0, c = 0;
+    for (c = 0; c < 12; c++)
+    {
+        soma += matriz[l][c];
+    }
+    return soma;
+}
 
 int main()
 {
     int mat[12][12];
-    int l, c;
-    // int soma = fSoma(mat, l);
+    int l;
+    int soma;
     FILE *arquivo;
+
+    arquivo = fopen("matriz_12x12.txt", "r");
+    for (int l = 0; l < 12; l++)
+    {
+        for (int c = 0; c < 12; c++)
+        {
+            fscanf(arquivo, "%i", &mat[l][c]);
+        }
+    }
+    fclose(arquivo);
 
     printf("Informe a linha da matriz entre 1 e 12: \n");
     scanf("%i", &l);
 
-    arquivo = fopen("matriz_12x12.txt", "r");
-    for (c = 0; c < 12; c++)
-    {
-        fscanf(arquivo, "%i", &mat[l][c]);
-        printf(mat[l][c]);
-    }
-    fclose(arquivo);
+    l--;
+
+    soma = fSoma(mat, l);
+
+    printf("Soma da linha: %i\n", soma);
 }
